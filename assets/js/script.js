cf4a062105dbabdebc900dbfd59dc99f3a981813
Blaze Lim <blazebentleycolim@gmail.com>
2021-03-08 10:51:25 -0800

    added pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// TODO add timer variable
+
+// TODO add question array
+
+// TODO add timer function
+
+// TODO add display highscores function
+
+// TODO add display questions functions
+
+// TODO add endgame function
+
+// TODO add answering questions function (removing previous questions and answer)
+
+// TODO add right wrong display 1 second, with "Question #"
+
+// TODO add localStorage functions and loading
